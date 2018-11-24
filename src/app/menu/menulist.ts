2b6e2328c7b9f9a1816ee3f19081d444879fc045
elvisBM <elvisbmartins@gmail.com>
2018-11-24 19:17:06 -0200

    criado menulist, const com array list de menus para ficar dinamico

---
+++ src/app/menu/menulist.ts
@@ -0,0 +1,5 @@
+export const MENU = [
+  {menu: 'Painel de Gestão', icon: 'icon-globo2.png', active: true, submenu: false},
+  {menu: 'Conta Digital', icon: 'icon-digital.png', active: false, submenu: false},
+  {menu: 'Ant. de Recebíveis', icon: 'icon-recebiveis.png', active: false, submenu: true}
+]
