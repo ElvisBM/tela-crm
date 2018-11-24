573e1a46bc21438828b450d52ff02fe36caf819b
elvisBM <elvisbmartins@gmail.com>
2018-11-24 14:33:42 -0200

    add components sidebar, header, infocontents no app components html e ts, para montar o layout base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HeaderComponent } from './header/header.component';
+import { InfocontentsComponent } from './infocontents/infocontents.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    HeaderComponent,
+    InfocontentsComponent
   ],
   imports: [
     BrowserModule
