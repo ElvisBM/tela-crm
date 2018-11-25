fb3a2a3e40602f3ef44745d6e3f6f2a1d8613423
elvisBM <elvisbmartins@gmail.com>
2018-11-25 12:39:34 -0200

    lista de bitcoins layout e estrutura base

--- src/app/list-coins/list-coins.component.ts
+++ src/app/list-coins/list-coins.component.ts
@@ -4,11 +4,13 @@ import { HttpClient } from "@angular/common/http";
 @Component({
   selector: 'app-list-coins',
   templateUrl: './list-coins.component.html',
-  styleUrls: ['./list-coins.component.scss']
+  styleUrls: ['./list-coins.component.scss'],
+  host: {class:'container-fluid'}
 })
 export class ListCoinsComponent implements OnInit{
   listCoins: Array<string> = [];
   constructor(private conexaoApi: HttpClient) {}
+
   ngOnInit(): void {
     this.conexaoApi.get('https://api.coinmarketcap.com/v1/ticker/').subscribe(data => {
       this.listCoins = data;
