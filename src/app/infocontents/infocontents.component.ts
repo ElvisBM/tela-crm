e3192d82ee4365b5fa270e3cb6ed88fd8d045215
elvisBM <elvisbmartins@gmail.com>
2018-11-25 02:00:56 -0200

    style infoContent e content do app para receber o conteudo atraves da roda

--- src/app/infocontents/infocontents.component.ts
+++ src/app/infocontents/infocontents.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-infocontents',
   templateUrl: './infocontents.component.html',
-  styleUrls: ['./infocontents.component.scss']
+  styleUrls: ['./infocontents.component.scss'],
+  host: {class:'container-fluid'}
 })
 export class InfocontentsComponent implements OnInit {
 
